7fd5090d9f6830bb7f283cd7ce28abdf7f72b066
cloudtech346 <cloudtech346@gmail.com>
2026-06-07 17:33:51 +0530

    Added Docker configuration and backend updates

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/testAPI", (req, res) => {
   res.send("API is working properly");
 });
 
-const PORT = 3000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
